7af6f539ac58b122222f2b7fe620daa6096ee3cc
Sofia Grigoreva <sofya.grigorevaa@gmail.com>
2025-12-13 17:39:01 +0300

    feat: add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,11 @@
+import type { StorybookConfig } from "@storybook/html-vite";
+
+const config: StorybookConfig = {
+  framework: {
+    name: "@storybook/html-vite",
+    options: {},
+  },
+  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+};
+
+export default config;
